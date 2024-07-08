556a166c2314eb5d5385522adc3b3f0c75e29edf
Abdullah Rasheed <bhatti253r@gmail.com>
2024-07-08 12:22:45 +0500

    refactor: add mandatory files in to it

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express from 'express'
-const app = express()
-const port = 3000
-app.use(express.static("public"))
-app.get("/", (req,res)=>{
-    res.send("<h2 style = 'color:red;'>work is done</h2>")
-})
+import express from "express";
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("<h2 style = 'color:red;'>work is done</h2>");
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
