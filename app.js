aad237b648bba3422595c8f4d188d32fb83352ac
Abdullah Rasheed <bhatti253r@gmail.com>
2024-08-31 20:25:21 +0500

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
   res.render("index.ejs");
 });
   app.get("/login", (req,res)=>{
