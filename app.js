30c1238866005e7ef3d88457aa94817ab95d2457
Abdullah Rasheed <bhatti253r@gmail.com>
2024-09-01 08:09:53 +0500

    "Updated route for homepage from '/home' to '/'"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
-app.get("/home", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index.ejs");
 });
   app.get("/login", (req,res)=>{
